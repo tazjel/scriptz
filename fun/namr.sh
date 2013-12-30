f91ecccf1f6e77188585579087c95545c3ca8323
nodiscc <nodiscc@gmail.com>
2013-12-30 16:27:24 +0100

    cleanup

--- fun/namr.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#Source: http://www.reddit.com/r/commandline/comments/1kubou/some_help_naming_your_web_site/
-grep '[^aeiou]er$' /usr/share/dict/words | shuf -n 1 | sed -r -e 's~er$~r~' -e 's~^(\w)~\u\1~'
