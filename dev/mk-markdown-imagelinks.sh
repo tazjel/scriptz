637c2ee1bd8499c6fb253e65ac7146d8503ba488
nodiscc <nodiscc@gmail.com>
2014-01-09 00:00:03 +0100

    dev/mk-markdown-imagelinks.sh: place thumbnails in same dir as original images

--- dev/mk-markdown-imagelinks.sh
+++ dev/mk-markdown-imagelinks.sh
@@ -19,8 +19,8 @@ do
 	else
 		IMAGE_EXTENSION=`echo "$IMAGE" | awk -F "." '{print $NF}'`
 		IMAGE_BASENAME=`basename "$IMAGE" "$IMAGE_EXTENSION"`
-		convert -thumbnail "$THUMBNAIL_SIZE"x "$IMAGE" "thumbs/${IMAGE_BASENAME}_thumb.${IMAGE_EXTENSION}"
-		echo "[![](${IMAGES_DIR}/thumbs/${IMAGE_BASENAME}_thumb.${IMAGE_EXTENSION})](${IMAGES_DIR}/${IMAGE})"
+		convert -thumbnail "$THUMBNAIL_SIZE"x "$IMAGE" "${IMAGE_BASENAME}_thumb.${IMAGE_EXTENSION}"
+		echo "[![](${IMAGES_DIR}/${IMAGE_BASENAME}_thumb.${IMAGE_EXTENSION})](${IMAGES_DIR}/${IMAGE})"
 	fi
 done
 
