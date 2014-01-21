ae1e4dd3825e72d59f228403668a691990589a77
nodiscc <nodiscc@gmail.com>
2014-01-21 19:52:41 +0100

    gif-to-ogv.sh: support converting multiple files, add -d option to delete original files

--- utility/conversion/gif-to-ogv.sh
+++ utility/conversion/gif-to-ogv.sh
@@ -2,16 +2,30 @@
 #Convert animated gifs to ogg video files
 #Source: https://github.com/nodiscc/scriptz
 #License: MIT (http://opensource.org/licenses/MIT)
+#TODO: make the output cleaner/more informative
+set -e
 
 
-FTYPE=`file --brief --mime pipad_gif_600_med.gif |awk '{print $1}'`
-if [ "$FTYPE" != "image/gif;" ]
-	then echo "File is not a gif image."; exit 1
+USAGE="USAGE: `basename $0` [ -d ] file1.gif file2.gif ...
+OPTIONS:    -d    delete original after conversion"
+
+
+if [ "$1" = "-d" ]
+then DELETE_ORIGINAL="true"; shift
+elif [ "$1" = "-h" -o "$1" = "--help" -o "$1" = "" ]
+then echo "$USAGE"; exit 0
 fi
 
-#TODO: make the output cleaner/more informative
-#convert the gif to a yuv stream
-mplayer "$1" -vo yuv4mpeg
-#Do the actual conversion. note that 25fps is arbitrary. Quality is set to 6 which gives correct results with a rather low bitrate
-avconv -r 25 -i stream.yuv -c:v libtheora -q:v 6 -r 25 "$1".ogv
-rm stream.yuv
+for file in $@
+do
+    FTYPE=`file --brief --mime "$file" |awk '{print $1}'`
+    if [ "$FTYPE" != "image/gif;" ] #Check if file is a gif
+        then echo "File $file is not a gif image."; exit 1
+    fi
+    mplayer "$file" -vo yuv4mpeg #convert the gif to a yuv stream
+    avconv -r 25 -i stream.yuv -c:v libtheora -q:v 6 -r 25 "$file".ogv #Do the actual conversion. note that 25fps is arbitrary. Quality is set to 6 which gives correct results with a rather low bitrate
+    rm stream.yuv
+    if [ "$DELETE_ORIGINAL" = "true" ]
+        then rm "$file"
+    fi
+done
